a7f19a0efd57a96bbe79c88c6b76c119d14f5eb0
shrutha s naik <shruthasnaik@gmail.com>
2023-08-17 16:07:49 +0530

    subtraction function added

--- python_progs/arith.py
+++ python_progs/arith.py
@@ -1,6 +1,10 @@
 def add(n1,n2):
     return(n1+n2)
 
+def sub(n1,n2):
+    return(n1-n2)
+
 a=int(input("Enter num1:"))
 b=int(input("Enter num2:"))
 print("sum:",add(a,b))
+print("subtraction:",sub(a,b))
